b947592e64dcf07a4668c61950bee9ee42bb48d8
David Duong <david_duong@apple.com>
2015-12-30 16:36:32 -0700

    RADD: 	- Added avatars 	- Added UserService

--- index.js
+++ index.js
@@ -2,36 +2,33 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
-var users = {};
+var userService = require('./lib/UserService')(io);
 
+var port = process.env.PORT || 3000;
 
 app.use(express.static('app'));
 
-
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('join room', function(newUser) {
-    socket.join(newUser.room);
-    if (!users[newUser.room]) {
-      users[newUser.room] = [];
-    }
-    users[newUser.room].push(newUser);
-    io.sockets.in(newUser.room).emit('new user joined', users[newUser.room]);
-    console.log('user ' + newUser.name + ' joined the room ' + newUser.room);
-    console.log('users in this room are:' + JSON.stringify(users[newUser.room]));
-  });
+  console.log('a user connected on socket: ' + JSON.stringify(socket.id));
+
+  socket
+    .on('chat message', function(msg){
+      console.log('message: ' + msg);
+      io.emit('chat message', msg);
+    })
+    .on('disconnect', function() {
+      console.log('user disconnected');
+    })
+    .on('join room', function(newUser) {
+      userService.registerUser(socket, newUser);
+      console.log('user ' + newUser.name + ' joined the room ' + newUser.room);
+      console.log('users in this room are:' + JSON.stringify(io.nsps['/'].adapter.rooms[newUser.room]));
+    });
 });
 
 
-http.listen(process.env.PORT, function(){
-  console.log('listening on *:' + process.env.PORT);
+http.listen(port, function() {
+  console.log('listening on *:' + port);
 });
 
 var questionPool = [
